6db3d96ea75fc08baf98c74cb7259e802db52a55
Sebastian.k8 <Sebastian.k8@P-0G-0B022-0382>
2022-03-30 12:40:51 +0200

    Konstruktur und Eigentschaft AnzahlSeiten und Gesichert implentiert

--- Wuerfelspiel/Wuerfel.cs
+++ Wuerfelspiel/Wuerfel.cs
@@ -4,5 +4,32 @@ namespace Wuerfelspiel
 {
     public class Wuerfel
     {
+        private int anzahlSeiten;
+        private bool gesichert;
+
+        public Wuerfel()
+        {
+
+        }
+
+        public Wuerfel(int anzahlSeiten)
+        {
+            this.anzahlSeiten = anzahlSeiten;
+        }
+
+
+        public int AnzahlSeiten
+        {
+            get { return anzahlSeiten; }
+        }
+
+
+        public bool Gesichert
+        {
+            get { return gesichert; }
+            set { gesichert = value; }
+        }
+
+
     }
 }

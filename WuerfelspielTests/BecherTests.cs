b3a597e11a4efe3655321e528eb0d943ccbc7471
Kieberts <sebikieb@gmail.com>
2022-04-04 20:34:45 +0200

    Add BecherTests + Beginning of Constructor Test

---
+++ WuerfelspielTests/BecherTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wuerfelspiel;
+
+namespace WuerfelspielTests
+{
+    [TestClass]
+    public class BecherTests
+    {
+        [TestMethod]
+        public void DummyTest()
+        {
+            // Dieser Test ist hier, um eine GitHub-Funktion zu testen. 
+            // Einfach löschen :)
+            Assert.IsTrue(true);// is tatsächlich true :O
+        }
+        
+        [TestMethod]
+        public void Becher_()
+        {
+            // Arrange
+            Becher becher;
+
+            // Act
+            becher = new Becher();
+
+            // Assert
+        }
+    }
+}
